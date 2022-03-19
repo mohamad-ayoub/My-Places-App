f4ad23c1f102c00d71ad2fe13bf030b05bb7cbc5
mohamad <mmayoub@gmail.com>
2022-03-19 17:02:28 +0200

    commit 03 : Connect to firebase

--- app/src/main/java/com/example/myplacesapplcation/LogoActivity.java
+++ app/src/main/java/com/example/myplacesapplcation/LogoActivity.java
@@ -1,19 +1,27 @@
 package com.example.myplacesapplcation;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LogoActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Calendar;
+
+public class LogoActivity extends AppCompatActivity {
+    public static String TAG = "LogoActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
         findViewById(R.id.appLogoImageView).setOnClickListener(gotoLoginActivity_OnClick);
+
+
+        FirebaseAuth.getInstance().signOut();
     }
 
     private View.OnClickListener gotoLoginActivity_OnClick = new View.OnClickListener() {
--- app/src/main/java/com/example/myplacesapplcation/MapActivity.java
+++ app/src/main/java/com/example/myplacesapplcation/MapActivity.java
@@ -9,13 +9,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Objects;
+
 public class MapActivity extends AppCompatActivity {
+    GoogleMap googleMap;
+    MapView mapView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
         findViewById(R.id.fabShowAddLocationDialog).setOnClickListener(fabShowAddLocationDialog_onClick);
         registerForContextMenu(findViewById(R.id.fabShowAddLocationDialog));
+
+        mapView = findViewById(R.id.placesMapView);
+
     }
 
     private View.OnClickListener fabShowAddLocationDialog_onClick = new View.OnClickListener() {
@@ -56,4 +70,6 @@ public class MapActivity extends AppCompatActivity {
         return true;
         //return super.onContextItemSelected(item);
     }
+
+
 }
